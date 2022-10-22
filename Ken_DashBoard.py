74e3cdb34721d24e567692a93217aac143e62d34
tbaolam <thienbao0712@gmail.com>
2022-10-21 23:21:42 -0700

    Showing Metrics in Aggregate

--- Ken_DashBoard.py
+++ Ken_DashBoard.py
@@ -1,4 +1,5 @@
 #import libs
+from audioop import add
 import pandas as pd
 import numpy as np
 import plotly.graph_objects as go
@@ -9,33 +10,64 @@ from datetime import datetime
 #load data
 @st.cache
 def load_data():
+    """ Loads in 4 dataframes and does light feature engineering"""
     df_agg = pd.read_csv('Aggregated_Metrics_By_Video.csv').iloc[1:,:]
     df_agg.columns = ['Video','Video title','Video publish time','Comments added','Shares','Dislikes','Likes',
-                        'Subscribers lost','Subscribers gained','RPM(USD)','CPM(USD)','Average % viewed','Average view duration',
-                        'Views','Watch time (hours)','Subscribers','Your estimated revenue (USD)','Impressions','Impressions ctr(%)']
+                      'Subscribers lost','Subscribers gained','RPM(USD)','CPM(USD)','Average % viewed','Average view duration',
+                      'Views','Watch time (hours)','Subscribers','Your estimated revenue (USD)','Impressions','Impressions ctr(%)']
     df_agg['Video publish time'] = pd.to_datetime(df_agg['Video publish time'])
     df_agg['Average view duration'] = df_agg['Average view duration'].apply(lambda x: datetime.strptime(x,'%H:%M:%S'))
     df_agg['Avg_duration_sec'] = df_agg['Average view duration'].apply(lambda x: x.second + x.minute*60 + x.hour*3600)
     df_agg['Engagement_ratio'] =  (df_agg['Comments added'] + df_agg['Shares'] +df_agg['Dislikes'] + df_agg['Likes']) /df_agg.Views
     df_agg['Views / sub gained'] = df_agg['Views'] / df_agg['Subscribers gained']
-    df_agg.sort_values('Video publish time', ascending = False, inplace = True)
+    df_agg.sort_values('Video publish time', ascending = False, inplace = True)    
     df_agg_sub = pd.read_csv('Aggregated_Metrics_By_Country_And_Subscriber_Status.csv')
     df_comments = pd.read_csv('Aggregated_Metrics_By_Video.csv')
     df_time = pd.read_csv('Video_Performance_Over_Time.csv')
     df_time['Date'] = pd.to_datetime(df_time['Date'])
-    return df_agg, df_agg_sub, df_comments, df_time
+    return df_agg, df_agg_sub, df_comments, df_time 
 
 #create dataframes from the function
 df_agg, df_agg_sub, df_comments, df_time = load_data()
-#engineer data
-df_agg_diff = df_agg.copy()
 
+#additional data engineering for aggregated data 
+df_agg_diff = df_agg.copy()
+metric_date_12mo = df_agg_diff['Video publish time'].max() - pd.DateOffset(months =12)
+median_agg = df_agg_diff[df_agg_diff['Video publish time'] >= metric_date_12mo].median(numeric_only=True)
+
+#create differences from the median for values 
+#Just numeric columns 
+numeric_cols = np.array((df_agg_diff.dtypes == 'float64') | (df_agg_diff.dtypes == 'int64'))
+df_agg_diff.iloc[:,numeric_cols] = (df_agg_diff.iloc[:,numeric_cols] - median_agg).div(median_agg)
 ## What metrics will be relevant?
 ## Difference from baseline
 ## Percent change by video
 
 #build dashboard
-add_sidebar = st.sidebar.selectbox('Aggregate or Individual Video', ('Aggregate Metrics', 'Individual Video Analysis'))
+add_sidebar = st.sidebar.selectbox('Aggregate or Individual Video', ('Aggregate Metrics','Individual Video Analysis'))
 
+## Total picutre
+if add_sidebar == 'Aggregate Metrics':
+    df_agg_metrics = df_agg[['Video publish time','Views','Likes','Subscribers','Shares','Comments added','RPM(USD)','Average % viewed',
+                             'Avg_duration_sec', 'Engagement_ratio','Views / sub gained']]
+    metric_date_6mo = df_agg_metrics['Video publish time'].max() - pd.DateOffset(months =6)
+    metric_date_12mo = df_agg_metrics['Video publish time'].max() - pd.DateOffset(months =12)
+    metric_medians6mo = df_agg_metrics[df_agg_metrics['Video publish time'] >= metric_date_6mo].median(numeric_only=True)
+    metric_medians12mo = df_agg_metrics[df_agg_metrics['Video publish time'] >= metric_date_12mo].median(numeric_only=True)
+
+    col1, col2, col3, col4, col5 = st.columns(5)
+    columns = [col1, col2, col3, col4, col5]
+
+    count = 0
+    for i in metric_medians6mo.index:
+        with columns[count]:
+            delta = (metric_medians6mo[i] - metric_medians12mo[i])/metric_medians12mo[i]
+            st.metric(label= i, value = round(metric_medians6mo[i],1), delta = "{:.2%}".format(delta))
+            count += 1
+            if count >= 5:
+                count = 0
+
+if add_sidebar == 'Individual Video Analysis':
+    st.write('Ind')
 
 
