02fb155651ff68e758a1999261d5ee0582ca9d95
tbaolam <thienbao0712@gmail.com>
2022-10-21 21:31:24 -0700

    Load data and created sidebar for view data option

--- Ken_DashBoard.py
+++ Ken_DashBoard.py
@@ -7,18 +7,35 @@ import streamlit as st
 from datetime import datetime
 
 #load data
-df_agg = pd.read_csv('Aggregated_Metrics_By_Video.csv').iloc[1:,:]
-df_agg.columns = ['Video','Video title','Video publish time','Comments added','Shares','Dislikes','Likes',
-                      'Subscribers lost','Subscribers gained','RPM(USD)','CPM(USD)','Average % viewed','Average view duration',
-                      'Views','Watch time (hours)','Subscribers','Your estimated revenue (USD)','Impressions','Impressions ctr(%)']
-df_agg['Video publish time'] = pd.to_datetime(df_agg['Video publish time'])
-df_agg['Average view duration'] = df_agg['Average view duration'].apply(lambda x: datetime.strptime(x,'%H:%M:%S'))
-df_agg['Avg_duration_sec'] = df_agg['Average view duration'].apply(lambda x: x.second + x.minute*60 + x.hour*3600)
-df_agg['Engagement_ratio'] =  (df_agg['Comments added'] + df_agg['Shares'] +df_agg['Dislikes'] + df_agg['Likes']) /df_agg.Views
-df_agg['Views / sub gained'] = df_agg['Views'] / df_agg['Subscribers gained']
-df_agg.sort_values('Video publish time', ascending = False, inplace = True)
-df_agg_sub = pd.read_csv('Aggregated_Metrics_By_Country_And_Subscriber_Status.csv')
-df_comments = pd.read_csv('Aggregated_Metrics_By_Video.csv')
-df_time = pd.read_csv('Video_Performance_Over_Time.csv')
-df_time['Date'] = pd.to_datetime(df_time['Date'])
+@st.cache
+def load_data():
+    df_agg = pd.read_csv('Aggregated_Metrics_By_Video.csv').iloc[1:,:]
+    df_agg.columns = ['Video','Video title','Video publish time','Comments added','Shares','Dislikes','Likes',
+                        'Subscribers lost','Subscribers gained','RPM(USD)','CPM(USD)','Average % viewed','Average view duration',
+                        'Views','Watch time (hours)','Subscribers','Your estimated revenue (USD)','Impressions','Impressions ctr(%)']
+    df_agg['Video publish time'] = pd.to_datetime(df_agg['Video publish time'])
+    df_agg['Average view duration'] = df_agg['Average view duration'].apply(lambda x: datetime.strptime(x,'%H:%M:%S'))
+    df_agg['Avg_duration_sec'] = df_agg['Average view duration'].apply(lambda x: x.second + x.minute*60 + x.hour*3600)
+    df_agg['Engagement_ratio'] =  (df_agg['Comments added'] + df_agg['Shares'] +df_agg['Dislikes'] + df_agg['Likes']) /df_agg.Views
+    df_agg['Views / sub gained'] = df_agg['Views'] / df_agg['Subscribers gained']
+    df_agg.sort_values('Video publish time', ascending = False, inplace = True)
+    df_agg_sub = pd.read_csv('Aggregated_Metrics_By_Country_And_Subscriber_Status.csv')
+    df_comments = pd.read_csv('Aggregated_Metrics_By_Video.csv')
+    df_time = pd.read_csv('Video_Performance_Over_Time.csv')
+    df_time['Date'] = pd.to_datetime(df_time['Date'])
+    return df_agg, df_agg_sub, df_comments, df_time
+
+#create dataframes from the function
+df_agg, df_agg_sub, df_comments, df_time = load_data()
+#engineer data
+df_agg_diff = df_agg.copy()
+
+## What metrics will be relevant?
+## Difference from baseline
+## Percent change by video
+
+#build dashboard
+add_sidebar = st.sidebar.selectbox('Aggregate or Individual Video', ('Aggregate Metrics', 'Individual Video Analysis'))
+
+
 
